ecdb6b6e24985309ac20de5558889bab149df7f8
erikwibowo <erikwibo@gmail.com>
2025-10-29 11:01:05 +0700

    refactor: update layout structure in LivenessDetectionTutorialScreen for improved readability and organization

--- lib/src/presentation/widgets/liveness_detection_tutorial_widget.dart
+++ lib/src/presentation/widgets/liveness_detection_tutorial_widget.dart
@@ -24,61 +24,88 @@ class _LivenessDetectionTutorialScreenState
       appBar: AppBar(
         title: const Text("Verifikasi Keaktifan"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Card(
-            child: ListView(
-              shrinkWrap: true,
-              children: [
-                ListTile(
-                  leading: CircleAvatar(
-                      child: Text('1',
-                          style: Theme.of(context).textTheme.titleMedium)),
-                  subtitle: const Text(
-                    "Pastikan pencahayaan di sekitar cukup terang agar wajah dapat terdeteksi dengan baik",
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: 12,
+          children: [
+            Card(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                        child: Text('1',
+                            style: Theme.of(context).textTheme.titleMedium)),
+                    subtitle: const Text(
+                      "Pastikan pencahayaan di sekitar cukup terang agar wajah dapat terdeteksi dengan baik",
+                    ),
+                    title: Text("Pencahayaan",
+                        style: Theme.of(context).textTheme.titleMedium),
                   ),
-                  title: Text("Pencahayaan",
-                      style: Theme.of(context).textTheme.titleMedium),
-                ),
-                ListTile(
-                  leading: CircleAvatar(
-                      child: Text('2',
-                          style: Theme.of(context).textTheme.titleMedium)),
-                  subtitle: const Text(
-                      "Pegang ponsel sejajar mata dan lihat lurus ke kamera",
-                      style: TextStyle()),
-                  title: Text("Pandangan Lurus",
-                      style: Theme.of(context).textTheme.titleMedium),
-                ),
-                ListTile(
-                  leading: CircleAvatar(
-                      child: Text('3',
-                          style: Theme.of(context).textTheme.titleMedium)),
-                  subtitle: Text(
-                    "Batas waktu yang diberikan untuk proses verifikasi sistem deteksi keaktifan adalah ${widget.duration ?? 45} detik",
+                  ListTile(
+                    leading: CircleAvatar(
+                        child: Text('2',
+                            style: Theme.of(context).textTheme.titleMedium)),
+                    subtitle: const Text(
+                        "Pegang ponsel sejajar mata dan lihat lurus ke kamera",
+                        style: TextStyle()),
+                    title: Text("Pandangan Lurus",
+                        style: Theme.of(context).textTheme.titleMedium),
                   ),
-                  title: Text("Batas Waktu",
-                      style: Theme.of(context).textTheme.titleMedium),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: FilledButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                  ListTile(
+                    leading: CircleAvatar(
+                        child: Text('3',
+                            style: Theme.of(context).textTheme.titleMedium)),
+                    subtitle: Text(
+                      "Batas waktu yang diberikan untuk proses verifikasi sistem deteksi keaktifan adalah ${widget.duration ?? 45} detik",
+                    ),
+                    title: Text("Batas Waktu",
+                        style: Theme.of(context).textTheme.titleMedium),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: FilledButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      icon: const Icon(Icons.camera_alt_outlined),
+                      onPressed: () => widget.onStartTap(),
+                      label: const Text(
+                        "Mulai Verifikasi keaktifan",
                       ),
                     ),
-                    icon: const Icon(Icons.camera_alt_outlined),
-                    onPressed: () => widget.onStartTap(),
-                    label: const Text(
-                      "Mulai Verifikasi keaktifan",
-                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  spacing: 8,
+                  children: [
+                    CircleAvatar(
+                      child: Icon(
+                        Icons.info_outline,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Layar informasi verifikasi keaktifan dapat dinonaktifkan pada pengaturan aplikasi.",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
